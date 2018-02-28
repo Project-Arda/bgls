8575e9a7cc89a7146503e75c32f87a02e555e629
ValarDragon <dojha12@gmail.com>
2018-02-27 16:21:39 -0800

    Abstract the choice of curve

--- bgls.go
+++ bgls.go
@@ -6,13 +6,7 @@ package bgls
 import (
 	"crypto/rand"
 
-	"github.com/dchest/blake2b"
-
 	"math/big"
-
-	"github.com/ethereum/go-ethereum/crypto/bn256"
-
-	"bytes"
 )
 
 //SigningKey wraps secret exponent
@@ -22,18 +16,17 @@ type SigningKey struct {
 
 //VerifyKey wraps public G2 curve point
 type VerifyKey struct {
-	key *bn256.G2
+	key Point2
 }
 
 //Authentication is a proof of a valid VerifyKey
 type Authentication struct {
-	t *bn256.G2
-	r *big.Int
+	sig *Signature
 }
 
 //Signature Wraps G1 curve point as signature
 type Signature struct {
-	sig *bn256.G1
+	sig Point1
 }
 
 //MultiSig holds set of keys and one message plus signature
@@ -50,93 +43,92 @@ type AggSig struct {
 	sig  *Signature
 }
 
-var g1 = new(bn256.G1).ScalarBaseMult(one)
-var g2 = new(bn256.G2).ScalarBaseMult(one)
-
 //KeyGen generates a SigningKey and VerifyKey
-func KeyGen() (*SigningKey, *VerifyKey, error) {
-	x, err := rand.Int(rand.Reader, bn256.Order)
+func KeyGen(curve CurveSystem) (*SigningKey, *VerifyKey, error) {
+	x, err := rand.Int(rand.Reader, curve.getG1Order())
 	if err != nil {
 		return nil, nil, err
 	}
-	sk, vk := LoadKey(x)
+	sk, vk := LoadKey(curve, x)
 	return sk, vk, nil
 }
 
 //LoadKey turns secret key into SigninKey and VerifyKey
-func LoadKey(x *big.Int) (*SigningKey, *VerifyKey) {
-	v := new(bn256.G2).ScalarBaseMult(x)
+func LoadKey(curve CurveSystem, x *big.Int) (*SigningKey, *VerifyKey) {
+	v, _ := curve.G2Mul(x, curve.GetG2())
 	vk := &VerifyKey{v}
 	sk := &SigningKey{x}
 	return sk, vk
 }
 
-//Authenticate generates an Authentication for a valid SigningKey/VerifyKey combo
-func Authenticate(x *SigningKey, v *VerifyKey) *Authentication {
-	k, _ := rand.Int(rand.Reader, bn256.Order)
-	t := new(bn256.G2).ScalarBaseMult(k)
-	H := blake2b.New256()
-	H.Write(v.key.Marshal())
-	H.Write(t.Marshal())
-	cb := make([]byte, 32)
-	H.Sum(cb)
-	c := new(big.Int)
-	c.SetBytes(cb)
-	c.Mod(c, bn256.Order)
-	r := new(big.Int).Mul(c, x.key)
-	r.Mod(r, bn256.Order)
-	r.Neg(r)
-	r.Add(r, k)
-	r.Mod(r, bn256.Order)
-	return &Authentication{t, r}
+// Authenticate generates an Authentication for a valid SigningKey/VerifyKey combo
+// It signs a verification key with x.
+func Authenticate(curve CurveSystem, x *SigningKey, v *VerifyKey) *Signature {
+	return AuthenticateCustHash(curve, x, v, curve.HashToG1)
+}
+
+// AuthenticateCustHash generates an Authentication for a valid SigningKey/VerifyKey combo
+// It signs a verification key with x. This runs with the specified hash function.
+func AuthenticateCustHash(curve CurveSystem, x *SigningKey, v *VerifyKey, hash func([]byte) Point1) *Signature {
+	m := curve.MarshalG2(v.key)
+	return x.SignCustHash(curve, m, hash)
 }
 
 //CheckAuthentication verifies that this VerifyKey is valid
-func CheckAuthentication(v *VerifyKey, a *Authentication) bool {
-	H := blake2b.New256()
-	H.Write(v.key.Marshal())
-	H.Write(a.t.Marshal())
-	cb := make([]byte, 32)
-	H.Sum(cb)
-	c := new(big.Int)
-	c.SetBytes(cb)
-	c.Mod(c, bn256.Order)
-	gr := new(bn256.G2).ScalarBaseMult(a.r)
-	vc := new(bn256.G2).ScalarMult(v.key, c)
-	tprime := new(bn256.G2).Add(gr, vc)
-	return g2Equals(a.t, tprime)
+func CheckAuthentication(curve CurveSystem, v *VerifyKey, authentication *Signature) bool {
+	return CheckAuthenticationCustHash(curve, v, authentication, curve.HashToG1)
+}
+
+//CheckAuthenticationCustHash verifies that this VerifyKey is valid, with the specified hash function
+func CheckAuthenticationCustHash(curve CurveSystem, v *VerifyKey, authentication *Signature, hash func([]byte) Point1) bool {
+	m := curve.MarshalG2(v.key)
+	return VerifyCustHash(curve, v, m, authentication, hash)
 }
 
 //Sign creates a signature on a message with a private key
-func (sk *SigningKey) Sign(m []byte) *Signature {
-	h := AltbnHashToCurve(m)
-	return &Signature{h.ScalarMult(h, sk.key)}
+func (sk *SigningKey) Sign(curve CurveSystem, m []byte) *Signature {
+	return sk.SignCustHash(curve, m, curve.HashToG1)
+}
+
+// SignCustHash creates a signature on a message with a private key, using
+// a supplied function to hash to g1.
+func (sk *SigningKey) SignCustHash(curve CurveSystem, m []byte, hash func([]byte) Point1) *Signature {
+	h := hash(m)
+	i, _ := curve.G1Mul(sk.key, h)
+	return &Signature{i}
 }
 
 // Verify checks that a signature is valid
-func Verify(vk *VerifyKey, m []byte, sig *Signature) bool {
-	h := AltbnHashToCurve(m)
-	return pairEquals(bn256.Pair(h, vk.key), bn256.Pair(sig.sig, g2))
+func Verify(curve CurveSystem, vk *VerifyKey, m []byte, sig *Signature) bool {
+	return VerifyCustHash(curve, vk, m, sig, curve.HashToG1)
+}
+
+// Verify checks that a signature is valid
+func VerifyCustHash(curve CurveSystem, vk *VerifyKey, m []byte, sig *Signature, hash func([]byte) Point1) bool {
+	h := hash(m)
+	p1, _ := curve.Pair(h, vk.key)
+	p2, _ := curve.Pair(sig.sig, curve.GetG2())
+	return curve.GTEquals(p1, p2)
 }
 
 // Aggregate turns a set of signatures into a single signature
-func Aggregate(sigs []*Signature) *Signature {
-	a := copyg1(sigs[0].sig)
+func Aggregate(curve CurveSystem, sigs []*Signature) *Signature {
+	a := curve.CopyG1(sigs[0].sig)
 	for _, s := range sigs[1:] {
-		a.Add(a, s.sig)
+		a, _ = curve.G1Add(a, s.sig)
 	}
 	return &Signature{a}
 }
 
 // Verify checks that all messages were signed by associated keys
 // Will fail under duplicate messages
-func (a AggSig) Verify() bool {
-	return VerifyAggregateSignature(a.sig, a.keys, a.msgs, false)
+func (a AggSig) Verify(curve CurveSystem) bool {
+	return VerifyAggregateSignature(curve, a.sig, a.keys, a.msgs, false)
 }
 
 // VerifyAggregateSignature verifies that the aggregated signature proves that all messages were signed by associated keys
 // Will fail under duplicate messages, unless allow duplicates is True.
-func VerifyAggregateSignature(aggsig *Signature, keys []*VerifyKey, msgs [][]byte, allowDuplicates bool) bool {
+func VerifyAggregateSignature(curve CurveSystem, aggsig *Signature, keys []*VerifyKey, msgs [][]byte, allowDuplicates bool) bool {
 	if len(keys) != len(msgs) {
 		return false
 	}
@@ -145,51 +137,34 @@ func VerifyAggregateSignature(aggsig *Signature, keys []*VerifyKey, msgs [][]byt
 			return false
 		}
 	}
-	e1 := bn256.Pair(aggsig.sig, g2)
-	h := AltbnHashToCurve(msgs[0])
-	e2 := bn256.Pair(h, keys[0].key)
+	e1, _ := curve.Pair(aggsig.sig, curve.GetG2())
+	h := curve.HashToG1(msgs[0])
+	e2, _ := curve.Pair(h, keys[0].key)
 	for i := 1; i < len(msgs); i++ {
-		h = AltbnHashToCurve(msgs[i])
-		e2.Add(e2, bn256.Pair(h, keys[i].key))
+		h = curve.HashToG1(msgs[i])
+		e3, _ := curve.Pair(h, keys[i].key)
+		e2, _ = curve.GTAdd(e3, e2)
 	}
-	return pairEquals(e1, e2)
+	return curve.GTEquals(e1, e2)
 }
 
 //Verify checks that a single message has been signed by a set of keys
 //vulnerable against chosen key attack, if keys have not been authenticated
-func (m MultiSig) Verify() bool {
-	return VerifyMultiSignature(m.sig, m.keys, m.msg)
+func (m MultiSig) Verify(curve CurveSystem) bool {
+	return VerifyMultiSignature(curve, m.sig, m.keys, m.msg)
 }
 
 // VerifyMultiSignature checks that the aggregate signature correctly proves that a single message has been signed by a set of keys,
 // vulnerable against chosen key attack, if keys have not been authenticated
-func VerifyMultiSignature(aggsig *Signature, keys []*VerifyKey, msg []byte) bool {
-	e1 := bn256.Pair(aggsig.sig, g2)
-	vs := copyg2(keys[0].key)
+func VerifyMultiSignature(curve CurveSystem, aggsig *Signature, keys []*VerifyKey, msg []byte) bool {
+	e1, _ := curve.Pair(aggsig.sig, curve.GetG2())
+	vs := curve.CopyG2(keys[0].key)
 	for i := 1; i < len(keys); i++ {
-		vs.Add(vs, keys[i].key)
+		vs, _ = curve.G2Add(vs, keys[i].key)
 	}
-	h := AltbnHashToCurve(msg)
-	e2 := bn256.Pair(h, vs)
-	return pairEquals(e1, e2)
-}
-
-func pairEquals(a, b *bn256.GT) bool {
-	return bytes.Equal(a.Marshal(), b.Marshal())
-}
-func g1Equals(a, b *bn256.G1) bool {
-	return bytes.Equal(a.Marshal(), b.Marshal())
-}
-func g2Equals(a, b *bn256.G2) bool {
-	return bytes.Equal(a.Marshal(), b.Marshal())
-}
-func copyg1(x *bn256.G1) *bn256.G1 {
-	p, _ := new(bn256.G1).Unmarshal(x.Marshal())
-	return p
-}
-func copyg2(x *bn256.G2) *bn256.G2 {
-	p, _ := new(bn256.G2).Unmarshal(x.Marshal())
-	return p
+	h := curve.HashToG1(msg)
+	e2, _ := curve.Pair(h, vs)
+	return curve.GTEquals(e1, e2)
 }
 
 func containsDuplicateMessage(msgs [][]byte) bool {
--- bgls_test.go
+++ bgls_test.go
@@ -11,6 +11,7 @@ import (
 )
 
 func TestAltbnHashToCurve(t *testing.T) {
+	curve := altBn128Inst
 	N := 10
 	msgs := make([][]byte, N)
 	for i := 0; i < N; i++ {
@@ -21,26 +22,26 @@ func TestAltbnHashToCurve(t *testing.T) {
 		testHashConsistency(AltbnKang12, "altbn kang12 hash", msgs[i], t)
 		testHashConsistency(AltbnBlake2b, "altbn blake2b hash", msgs[i], t)
 
-		p1 := AltbnHashToCurve(msgs[i])
-		p2 := AltbnHashToCurve(msgs[i])
-		if !g1Equals(p1, p2) {
+		p1 := curve.HashToG1(msgs[i])
+		p2 := curve.HashToG1(msgs[i])
+		if !curve.G1Equals(p1, p2) {
 			t.Error("inconsistent results in Altbn HashToCurve")
 		}
 	}
 }
 
-func TestBls12Hashing(t *testing.T) {
-	// Tests consistency
-	N := 10
-	msgs := make([][]byte, N)
-	for i := 0; i < N; i++ {
-		msgs[i] = make([]byte, N)
-		_, _ = rand.Read(msgs[i])
-		testHashConsistency(Bls12Sha3, "bls12 sha3 hash", msgs[i], t)
-		testHashConsistency(Bls12Kang12, "bls12 kang12 hash", msgs[i], t)
-		testHashConsistency(Bls12Blake2b, "bls12 blake2b hash", msgs[i], t)
-	}
-}
+// func TestBls12Hashing(t *testing.T) {
+// 	// Tests consistency
+// 	N := 10
+// 	msgs := make([][]byte, N)
+// 	for i := 0; i < N; i++ {
+// 		msgs[i] = make([]byte, N)
+// 		_, _ = rand.Read(msgs[i])
+// 		testHashConsistency(Bls12Sha3, "bls12 sha3 hash", msgs[i], t)
+// 		testHashConsistency(Bls12Kang12, "bls12 kang12 hash", msgs[i], t)
+// 		testHashConsistency(Bls12Blake2b, "bls12 blake2b hash", msgs[i], t)
+// 	}
+// }
 
 func testHashConsistency(hashFunc func(message []byte) (p1, p2 *big.Int), hashname string, msg []byte, t *testing.T) {
 	x1, y1 := hashFunc(msg)
@@ -51,6 +52,7 @@ func testHashConsistency(hashFunc func(message []byte) (p1, p2 *big.Int), hashna
 }
 
 func TestEthereumHash(t *testing.T) {
+	curve := altBn128Inst
 	// Tests Altbn hash to curve against known solidity test case.
 	a := []byte{116, 101, 115, 116}
 	x, y := AltbnKeccak3(a)
@@ -59,19 +61,20 @@ func TestEthereumHash(t *testing.T) {
 	if x.Cmp(expX) != 0 || y.Cmp(expY) != 0 {
 		t.Error("Hash does not match known Ethereum Output")
 	}
-	pt := AltbnHashToCurve(a)
-	x2, y2 := AltbnG1ToCoord(pt)
+	pt := curve.HashToG1(a)
+	x2, y2 := pt.g1ToAffineCoords()
 	if x.Cmp(x2) != 0 || y.Cmp(y2) != 0 {
 		t.Error("Conversion of point to coordinates is not working")
 	}
 }
 
 func TestSingleSigner(t *testing.T) {
-	sk, vk, err := KeyGen()
+	curve := altBn128Inst
+	sk, vk, err := KeyGen(curve)
 	if err != nil {
 		t.Error("Key generation failed")
 	}
-	if !CheckAuthentication(vk, Authenticate(sk, vk)) {
+	if !CheckAuthentication(curve, vk, Authenticate(curve, sk, vk)) {
 		t.Error("Key Authentication failed")
 	}
 	d := make([]byte, 64)
@@ -79,15 +82,15 @@ func TestSingleSigner(t *testing.T) {
 	if err != nil {
 		t.Error("test data generation failed")
 	}
-	sig := sk.Sign(d)
-	if !Verify(vk, d, sig) {
+	sig := sk.Sign(curve, d)
+	if !Verify(curve, vk, d, sig) {
 		t.Error("Signature verification failed")
 	}
 
-	sigTmp := copyg1(sig.sig)
-	sigTmp.Add(sigTmp, g1)
+	sigTmp := curve.CopyG1(sig.sig)
+	sigTmp, _ = curve.G1Add(sigTmp, curve.GetG1())
 	sig2 := &Signature{sigTmp}
-	if Verify(vk, d, sig2) {
+	if Verify(curve, vk, d, sig2) {
 		t.Error("Signature verification succeeding when it shouldn't")
 	}
 
@@ -95,6 +98,7 @@ func TestSingleSigner(t *testing.T) {
 }
 
 func TestAggregation(t *testing.T) {
+	curve := altBn128Inst
 	N := 6
 	Size := 32
 	msgs := make([][]byte, N+1)
@@ -104,41 +108,44 @@ func TestAggregation(t *testing.T) {
 		msgs[i] = make([]byte, Size)
 		_, _ = rand.Read(msgs[i])
 
-		sk, vk, _ := KeyGen()
-		sig := sk.Sign(msgs[i])
+		sk, vk, _ := KeyGen(curve)
+		sig := sk.Sign(curve, msgs[i])
 		pubkeys[i] = vk
 		sigs[i] = sig
 	}
-	aggSig := Aggregate(sigs[:N])
-	if !VerifyAggregateSignature(aggSig, pubkeys[:N], msgs[:N], false) {
+	aggSig := Aggregate(curve, sigs[:N])
+	if !VerifyAggregateSignature(curve, aggSig, pubkeys[:N], msgs[:N], false) {
 		t.Error("Aggregate Signature verification failed")
 	}
-	if VerifyAggregateSignature(aggSig, pubkeys[:N-1], msgs[:N], false) {
+	if VerifyAggregateSignature(curve, aggSig, pubkeys[:N-1], msgs[:N], false) {
 		t.Error("Aggregate Signature verification succeeding without enough pubkeys")
 	}
-	skf, vkf, _ := KeyGen()
+	skf, vkf, _ := KeyGen(curve)
 	pubkeys[N] = vkf
-	sigs[N] = skf.Sign(msgs[0])
+	sigs[N] = skf.Sign(curve, msgs[0])
 	msgs[N] = msgs[0]
-	aggSig = Aggregate(sigs)
-	if VerifyAggregateSignature(aggSig, pubkeys, msgs, false) {
+	aggSig = Aggregate(curve, sigs)
+	if VerifyAggregateSignature(curve, aggSig, pubkeys, msgs, false) {
 		t.Error("Aggregate Signature succeeding with duplicate messages with allow duplicates being false")
 	}
-	if !VerifyAggregateSignature(aggSig, pubkeys, msgs, true) {
+	if !VerifyAggregateSignature(curve, aggSig, pubkeys, msgs, true) {
 		t.Error("Aggregate Signature failing with duplicate messages with allow duplicates")
 	}
-	if VerifyAggregateSignature(aggSig, pubkeys[:N], msgs[:N], false) {
+	if VerifyAggregateSignature(curve, aggSig, pubkeys[:N], msgs[:N], false) {
 		t.Error("Aggregate Signature succeeding with invalid signature")
 	}
 	msgs[0] = msgs[1]
 	msgs[1] = msgs[N]
-	aggSig = Aggregate(sigs[:N])
-	if VerifyAggregateSignature(aggSig, pubkeys[:N], msgs[:N], false) {
+	aggSig = Aggregate(curve, sigs[:N])
+	if VerifyAggregateSignature(curve, aggSig, pubkeys[:N], msgs[:N], false) {
 		t.Error("Aggregate Signature succeeded with messages 0 and 1 switched")
 	}
+
+	// TODO Add tests to make sure there is no mutation
 }
 
 func TestMultiSig(t *testing.T) {
+	curve := altBn128Inst
 	Tests := 5
 	Size := 32
 	Signers := 10
@@ -148,22 +155,22 @@ func TestMultiSig(t *testing.T) {
 		signers := make([]*VerifyKey, Signers)
 		sigs := make([]*Signature, Signers)
 		for j := 0; j < Signers; j++ {
-			sk, vk, _ := KeyGen()
-			sigs[j] = sk.Sign(msg)
+			sk, vk, _ := KeyGen(curve)
+			sigs[j] = sk.Sign(curve, msg)
 			signers[j] = vk
 		}
-		aggSig := Aggregate(sigs)
-		if !VerifyMultiSignature(aggSig, signers, msg) {
+		aggSig := Aggregate(curve, sigs)
+		if !VerifyMultiSignature(curve, aggSig, signers, msg) {
 			t.Error("Aggregate MultiSig verification failed")
 		}
 		msg2 := make([]byte, Size)
 		_, _ = rand.Read(msg2)
-		if VerifyMultiSignature(aggSig, signers, msg2) {
+		if VerifyMultiSignature(curve, aggSig, signers, msg2) {
 			t.Error("Aggregate MultiSig verification succeeded on incorrect msg")
 		}
-		_, vkf, _ := KeyGen()
+		_, vkf, _ := KeyGen(curve)
 		signers[0] = vkf
-		if VerifyMultiSignature(aggSig, signers, msg) {
+		if VerifyMultiSignature(curve, aggSig, signers, msg) {
 			t.Error("Aggregate MultiSig verification succeeded on incorrect signers")
 		}
 	}
@@ -171,8 +178,9 @@ func TestMultiSig(t *testing.T) {
 
 func BenchmarkKeygen(b *testing.B) {
 	b.ResetTimer()
+	curve := altBn128Inst
 	for i := 0; i < b.N; i++ {
-		_, _, res := KeyGen()
+		_, _, res := KeyGen(curve)
 		if res != nil {
 			b.Error("key gen failure")
 		}
@@ -180,6 +188,7 @@ func BenchmarkKeygen(b *testing.B) {
 }
 
 func BenchmarkAltBnHashToCurve(b *testing.B) {
+	curve := altBn128Inst
 	ms := make([][]byte, b.N)
 	for i := 0; i < b.N; i++ {
 		ms[i] = make([]byte, 64)
@@ -187,33 +196,35 @@ func BenchmarkAltBnHashToCurve(b *testing.B) {
 	}
 	b.ResetTimer()
 	for i := 0; i < b.N; i++ {
-		AltbnHashToCurve(ms[i])
+		curve.HashToG1(ms[i])
 	}
 }
 
 func BenchmarkSigning(b *testing.B) {
+	curve := altBn128Inst
 	sks := make([]*SigningKey, b.N)
 	ms := make([][]byte, b.N)
 	for i := 0; i < b.N; i++ {
 		ms[i] = make([]byte, 64)
 		_, _ = rand.Read(ms[i])
-		sk, _, _ := KeyGen()
+		sk, _, _ := KeyGen(curve)
 		sks[i] = sk
 	}
 	b.ResetTimer()
 	for i := 0; i < b.N; i++ {
-		_ = sks[i].Sign(ms[i])
+		_ = sks[i].Sign(curve, ms[i])
 	}
 }
 
 func BenchmarkVerification(b *testing.B) {
+	curve := altBn128Inst
 	message := make([]byte, 64)
 	_, _ = rand.Read(message)
-	sk, vk, _ := KeyGen()
-	sig := sk.Sign(message)
+	sk, vk, _ := KeyGen(curve)
+	sig := sk.Sign(curve, message)
 	b.ResetTimer()
 	for i := 0; i < b.N; i++ {
-		if !Verify(vk, message, sig) {
+		if !Verify(curve, vk, message, sig) {
 			b.Error("verification failed")
 		}
 	}
@@ -224,23 +235,83 @@ var sgs []*Signature
 var msg []byte
 
 func TestMain(m *testing.M) {
+	curve := altBn128Inst
 	vks = make([]*VerifyKey, 2048)
 	sgs = make([]*Signature, 2048)
 	msg = make([]byte, 64)
 	_, _ = rand.Read(msg)
 	for i := 0; i < 2048; i++ {
-		sk, vk, _ := KeyGen()
+		sk, vk, _ := KeyGen(curve)
 		vks[i] = vk
-		sgs[i] = sk.Sign(msg)
+		sgs[i] = sk.Sign(curve, msg)
 	}
 	os.Exit(m.Run())
 }
 
+func TestKnownCases(t *testing.T) {
+	curve := altBn128Inst
+	N := 3
+	msgs := make([][]byte, N)
+	msg1 := []byte{65, 20, 86, 143, 250}
+	msg2 := []byte{157, 76, 30, 64, 128}
+	msg3 := []byte{202, 255, 227, 59, 238}
+	x1, _ := new(big.Int).SetString("7830752896741750908830464020410322281763657818307273013205711220156049734883", 10)
+	x2, _ := new(big.Int).SetString("10065703961787583059826108098259128135713944641698809475150397710106034167549", 10)
+	x3, _ := new(big.Int).SetString("17145080297596291172729378766677038070724014074212589728874454474449054012678", 10)
+
+	pubkeys := make([]*VerifyKey, N)
+	sk1, vk1 := LoadKey(curve, x1)
+	sk2, vk2 := LoadKey(curve, x2)
+	sk3, vk3 := LoadKey(curve, x3)
+	msgs[0] = msg1
+	msgs[1] = msg2
+	msgs[2] = msg3
+
+	pubkeys[0] = vk1
+	pubkeys[1] = vk2
+	pubkeys[2] = vk3
+
+	sigGen1 := sk1.Sign(curve, msgs[0])
+	sigGen2 := sk2.Sign(curve, msgs[1])
+	sigGen3 := sk3.Sign(curve, msgs[2])
+	sigVal1_1, _ := new(big.Int).SetString("21637350149051642305293442272499488026428127697128429631193536777535027009518", 10)
+	sigVal1_2, _ := new(big.Int).SetString("149479762519169687769683150632580363857094522511606512652585818657412262489", 10)
+	sigVal2_1, _ := new(big.Int).SetString("14834848655731874780751719195269704123719987185153910215596714529658047741046", 10)
+	sigVal2_2, _ := new(big.Int).SetString("5847895190688397897156144807293187828750812390735163763226617490736304595451", 10)
+	sigVal3_1, _ := new(big.Int).SetString("21239057713889019692075876723610689006006025737755828182426488764514117409847", 10)
+	sigVal3_2, _ := new(big.Int).SetString("11967902298809667109716532536825395835657143208987520118971083760489593281874", 10)
+	sigChk1, _ := curve.MakeG1Point(sigVal1_1, sigVal1_2)
+	sigChk2, _ := curve.MakeG1Point(sigVal2_1, sigVal2_2)
+	sigChk3, _ := curve.MakeG1Point(sigVal3_1, sigVal3_2)
+
+	if !curve.G1Equals(sigChk1, sigGen1.sig) || !curve.G1Equals(sigChk2, sigGen2.sig) || !curve.G1Equals(sigChk3, sigGen3.sig) {
+		t.Error("Recreating message signatures from known test cases failed")
+	}
+
+	sigs := make([]*Signature, N)
+	sigs[0] = sigGen1
+	sigs[1] = sigGen2
+	sigs[2] = sigGen3
+
+	aggSig1, _ := new(big.Int).SetString("12682380538491839124790562586247816360937861029087546329767912056050859037239", 10)
+	aggSig2, _ := new(big.Int).SetString("5755139208159515629159661524903000057840676877654799839167369795924360592246", 10)
+	aggSigChk, _ := curve.MakeG1Point(aggSig1, aggSig2)
+
+	aggSig := Aggregate(curve, sigs)
+	if !curve.G1Equals(aggSigChk, aggSig.sig) {
+		t.Error("Aggregate signature does not match the known test case.")
+	}
+	if !VerifyAggregateSignature(curve, aggSig, pubkeys, msgs, false) {
+		t.Error("Aggregate Signature verification failed")
+	}
+}
+
 func benchmulti(b *testing.B, k int) {
-	multisig := MultiSig{vks[:k], Aggregate(sgs[:k]), msg}
+	curve := altBn128Inst
+	multisig := MultiSig{vks[:k], Aggregate(curve, sgs[:k]), msg}
 	b.ResetTimer()
 	for i := 0; i < b.N; i++ {
-		if !multisig.Verify() {
+		if !multisig.Verify(curve) {
 			b.Error("MultiSig verification failed")
 		}
 	}
@@ -271,19 +342,20 @@ func BenchmarkMultiVerification2048(b *testing.B) {
 }
 
 func BenchmarkAggregateVerification(b *testing.B) {
+	curve := altBn128Inst
 	verifkeys := make([]*VerifyKey, b.N)
 	sigs := make([]*Signature, b.N)
 	messages := make([][]byte, b.N)
 	for i := 0; i < b.N; i++ {
 		messages[i] = make([]byte, 64)
 		_, _ = rand.Read(messages[i])
-		sk, vk, _ := KeyGen()
+		sk, vk, _ := KeyGen(curve)
 		verifkeys[i] = vk
-		sigs[i] = sk.Sign(messages[i])
+		sigs[i] = sk.Sign(curve, messages[i])
 	}
-	aggsig := AggSig{verifkeys, messages, Aggregate(sigs)}
+	aggsig := AggSig{verifkeys, messages, Aggregate(curve, sigs)}
 	b.ResetTimer()
-	if !aggsig.Verify() {
+	if !aggsig.Verify(curve) {
 		b.Error("Aggregate verificaton failed")
 	}
 }
